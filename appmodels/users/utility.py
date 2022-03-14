cbb32f205500fd9124f024128c45df7d5145c4ea
bethlehem-tadesse <bethlehemtadesse7@gmail.com>
2022-03-14 16:46:00 +0300

    more utility methods

--- appmodels/users/utility.py
+++ appmodels/users/utility.py
@@ -24,3 +24,21 @@ def save_picture(form_picture):
     i.save(picture_path)
     
     return picture_fn
+
+def send_reset_email(user):
+    """
+    A function that sends a reset email with time dependent reset link
+    """
+    token = user.get_reset_token()
+    msg = EmailMessage()
+    message = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    msg.set_content(message)
+    msg['Subject'] = 'Password Reset Request'
+    msg['From'] = os.environ.get('MAIL_USERNAME')
+    msg['To'] = user.email
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(os.environ.get('MAIL_USERNAME'), os.environ.get('MAIL_PASSWORD'))
+    server.send_message(msg)
